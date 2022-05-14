b3ab8fa9aa699c312ff6e328040b666b6d253f98
angelo-loria <angelosloria@gmail.com>
2022-05-14 16:02:41 -0500

    add next page logic

--- test/specs/scraper.spec.js
+++ test/specs/scraper.spec.js
@@ -11,17 +11,20 @@ describe('product search and scrape', () => {
 
   it('scrapes search results', async () => {
     const numberOfProductsFound = await searchResultsPage.getNumberOfProductsFound()
-
-    // default number of items displayed === 12
-    if (await numberOfProductsFound > 12) {
-      await searchResultsPage.updateNumberOfProductsDisplayed(96)
-    }
-
     const timestamp = new Date().toISOString().substring(0, 19).replace(/:/g, '')
     const filePath = `productDetails_${timestamp}.txt`
 
     console.log(`scraping ${numberOfProductsFound} products to ${filePath}`)
     await searchResultsPage.writeProductDetailsToFile(filePath)
+
+    // default number of items shown is 12
+    // if more than 12 items, go to next page
+    if (numberOfProductsFound > 12) {
+      while (!await searchResultsPage.isLastPage()) {
+        await searchResultsPage.openNextPage()
+        await searchResultsPage.writeProductDetailsToFile(filePath)
+      }
+    }
 
     expect(await searchResultsPage.fileExists(filePath)).toBe(true)
   })
